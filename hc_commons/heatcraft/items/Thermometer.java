a1bdf0d144ffab3c89fa07885faecd5d84af615c
Ward van der Velden <wardtuur224@live.nl>
2013-03-31 11:48:54 +0200

    Thermometer edit

--- hc_commons/heatcraft/items/Thermometer.java
+++ hc_commons/heatcraft/items/Thermometer.java
@@ -27,9 +27,32 @@ public class Thermometer extends Item {
             Float temp2 = world.getBiomeGenForCoords(x, z).temperature;
             String temperature = Float.toString(temp2);
             String msg = "§2Your current temperature is §4" + temperature + "§2...";
+            String msg2 = "Temperature: " + Float.toString(convertTempToCelsius(temp2)) + " degrees Celsius";
             player.sendChatToPlayer(msg);
+            player.sendChatToPlayer(msg2);
         }
         
         return itemStack;
+    }
+    
+    private float convertTempToCelsius(float temperature) {
+    	// average temperate of an Texas plain is 15 Celsius
+    	// minecraft temperature of plains is 0.8
+    	// factor is 18.5
+    	float t = temperature * 18.5F;
+    	
+    	if (t > 25.0F) {
+    		float d = t - 25.0F;
+    		t = t - d;
+    		d = d * 0.45F;
+    		t = t + d;
+    	}
+    	
+    	if (temperature < 1) {
+    		float nt = -1 + temperature;
+    		t = nt * 12.8F;
+    	}
+    	
+    	return t;
     }
 }
